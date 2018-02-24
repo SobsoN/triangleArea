36e8571f4240a9b2911425a78a766072a2636ded
Grzegorz Sobczyk <grzegorzsobczykdev@gmail.com>
2018-02-24 20:25:15 +0100

    Add everything

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+//POLE TRÓJKĄTA
+const a = prompt('Write an a of triangle'),
+	height = prompt('Write a height of triangle');
+const triangleArea = (height*a/2);
+console.log('Triangle field with base a:  '+ a +' and height equal to: '+height+ ' equals: '+triangleArea);
